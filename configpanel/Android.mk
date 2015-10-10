srisurya95/android_device_oneplus_oneplus2

configpanel: keyhandler: Add makefile guard

configpanel/Android.mk
@@ -15,6 +15,8 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),oneplus2)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -22,3 +24,4 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := ConfigPanel
 
 include $(BUILD_PACKAGE)
+endif

keyhandler/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
+ifeq ($(TARGET_DEVICE),oneplus2)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := com.cyanogenmod.keyhandler
@@ -8,4 +9,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
 
 include $(BUILD_JAVA_LIBRARY)
+endif
 

MoKee/android_device_oneplus_tocino

Change-Id: I4ed8699f41fc8befe78e9f5b13bcadb572767db5

configpanel/Android.mk
@@ -15,6 +15,8 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),tocino)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -22,3 +24,4 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := ConfigPanel
 
 include $(BUILD_PACKAGE)
+endif

keyhandler/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
+ifeq ($(TARGET_DEVICE),tocino)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := com.cyanogenmod.keyhandler
@@ -8,4 +9,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
 
 include $(BUILD_JAVA_LIBRARY)
+endif
 