7c5fc915753e20d9d93858ee52b8ef593573b3c5
martincz <martincz@mokeedev.com>
2015-10-21 14:01:58 +0800

    tocino: switch back and recent key
    Change-Id: Ia2960caeafd0549f387e6aae102267acc75f1932

--- mokee.mk
+++ mokee.mk
@@ -25,6 +25,6 @@ $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 PRODUCT_NAME := mk_tocino
 PRODUCT_DEVICE := tocino
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_MODEL := ONE A2001
+PRODUCT_MODEL := A2001
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
