5b6e8f7e88d95dc002ca980387fef77a3cf96ab0
XiNGRZ <chenxingyu92@gmail.com>
2017-07-28 02:39:02 +0800

    oneplus2: Add OneplusPocketMode
    * To prevent unexpected fingerprint unlocking fails
    
    Change-Id: I8395bb19a3d012613b144b68021beaf8a825ae81

--- device.mk
+++ device.mk
@@ -134,7 +134,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    fingerprintd
+    fingerprintd \
+    OneplusPocketMode
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
